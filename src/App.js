23203dab6a062d5d498601ea6556f12798b378dc
Papryk <kawa.patryk@gmail.com>
2021-02-26 10:43:40 +0100

    Routing + navigation added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import {Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import {LinkContainer} from 'react-router-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="container row">
+                <Navbar bg="dark" variant="dark">
+                    <Navbar.Brand href="#home">Noteapp</Navbar.Brand>
+                    <Nav className="mr-auto">
+                        <LinkContainer>
+                            <Nav.Link>All</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer>
+                            <Nav.Link>Home</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer>
+                            <Nav.Link>Work</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer>
+                            <Nav.Link>Personal</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer>
+                            <Nav.Link>ADD NOTE</Nav.Link>
+                        </LinkContainer>
+                    </Nav>+++
+                    <Form inline>
+                        <FormControl type="text" placeholder="Search" className="mr-sm-2"/>
+                        <Button variant="outline-info">Search</Button>
+                    </Form>
+                </Navbar>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
